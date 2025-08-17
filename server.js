6d44485018667ae027732cdae55d5bd757d1fa14
Alaa allam <alaa.lekro@gmail.com>
2025-08-18 01:30:32 +0300

    test number 2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('THIS IS TEST NUMBER 1');
+    res.send('THIS IS TEST NUMBER 2');
 });
 
 app.listen(3000, () => {
